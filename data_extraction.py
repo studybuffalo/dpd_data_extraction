bb98b02f48411df3d8e37e1f4055f26efebcbdbb
studybuffalo <studybuffalo@gmail.com>
2017-02-20 11:17:48 -0700

    Updating the archive extraction function for Python 3 and refactored a bit

--- data_extraction.py
+++ data_extraction.py
@@ -169,6 +169,165 @@ def download_zips(suffixList, date):
     
     print ("\n")
 
+def unzip_zips(suffixList, date, fileList):
+    # Create the folder to hold the data extracts
+    zipsLocation = root.child("dpd_data_extracts", date)
+
+    for suffix in suffixList:
+        # Set the file name for the downloads
+        if suffix == "":
+            zipName = "allfiles.zip"
+        else:
+            zipName = "allfiles_" + suffix + ".zip"
+        
+        zipPath  = zipsLocation.child(zipName).absolute()
+    
+        # Unzip the zips
+        print (("Unzipping %s... " % zipName), end="")
+        archive = zipfile.ZipFile(zipPath, 'r')
+        
+        # Cycles through all the files and extracts the data
+        for fileName in fileList:
+            # Generates the file name
+            if suffix == "":
+                fileName = fileName + ".txt"
+            else:
+                fileName = fileName + "_" + suffix + ".txt"
+            
+            # Extracts the text file from the archive
+            archive.extract(fileName, path=zipsLocation.absolute())
+        
+        archive.close()
+        print ("Completed!")
+
+    print ("\n")
+
+def parse_files():
+    """
+    # Creates folder for parsed text files if necessary
+    extractLocation = os.path.join(scriptDir, "Parsed_Data", date)
+    extractLocation = os.path.normpath(extractLocation)
+		
+    if not os.path.exists(extractLocation):
+	    os.mkdir(extractLocation)
+		
+    # Creates a new text file or deletes previous data within file
+    extractName = os.path.join(extractLocation, fileName)
+    extractName = os.path.normpath(extractName)
+		
+    extractFile = codecs.open(extractName, encoding='latin-1', mode='w')
+    extractFile.truncate()
+		
+    # Determining number of lines in text file for progress bar
+    length = file_len(fileName)
+    i = 1
+    n = 1
+    percent = 0
+		
+    # Opens file with 'latin-1' encoding
+    with codecs.open(fileName, encoding='latin-1') as file:
+	    for line in file:
+		    # Reviews and formats each line as required
+		    parsedText = parse(fileName, line)
+				
+		    # Writes each line to a temporary file (will ultimately upload to database)
+		    try:
+			    extractFile.write("%s\n" % parsedText)
+		    except:
+			    extractFile.write("ERROR PARSING ENTRY\n")
+				
+		    # Progress bar
+		    percent = 100.00 * i / length
+				
+		    if percent != 100:
+			    print("Parsing [%s%s] %.2f%%\r" % 
+					    ((n - 1) * "#", (51 - n) * " ", percent)),
+			    sys.stdout.flush()
+					
+			    n = n + 1 if percent > n * 2 else n
+		    else:
+			    print("Parsing [%s] Complete!" % ("#" * 50))
+				
+		    i += 1
+		
+    # Closes the extract file
+    try:
+	    print("Closing %s\n" % fileName)
+	    os.remove(fileName)
+    except OSError, e:
+	    print("Error: %s - %s." % (e.fileName, e.strerror))
+	"""
+    return ""
+
+def upload_data(data):
+    """
+    #Connect to database
+    #List of database tables
+    tableList = ["comp", "comp_ap", "comp_ia",
+			     "drug", "drug_ap", "drug_ia",
+			     "form", "form_ap", "form_ia",
+			     "ingred", "ingred_ap", "ingred_ia",
+			     "package", "package_ap", "package_ia",
+			     "pharm", "pharm_ap", "pharm_ia",
+			     "route", "route_ap", "route_ia",
+			     "schedule", "schedule_ap", "schedule_ia",
+			     "status", "status_ap", "status_ia",
+			     "ther", "ther_ap", "ther_ia",
+			     "vet", "vet_ap", "vet_ia"]
+			 
+    print("Establishing database connection")
+    conn = MySQLdb.connect(user='USERNAME', 
+					       passwd='PASSWORD', 
+					       db='DATABASE', 
+					       host='HOST',
+					       charset='latin1',
+					       use_unicode=True)
+    cursor = conn.cursor()
+
+    #Cycles through and uploads each entry to the database
+    for tableName in tableList:
+    
+        print("Uploading to '%s' table..." % tableName),
+	
+        #Truncates table to prepare for new entries
+        try:
+            cursor.execute("TRUNCATE %s" % tableName)
+            conn.commit()
+        except MySQLdb.Error, e:
+            print ("Error Truncating Table: %s" % e)
+            pass
+	
+        #Assembling path to parsed text file
+        fileName = os.path.join(scriptDir, "Parsed_Data", date, tableName + ".txt")
+        fileName = os.path.normpath(fileName)
+	
+        tempList = []
+        length = file_len(fileName)
+        i = 1
+        n = 1
+	
+        # Opens file with 'latin-1' encoding
+        with codecs.open(fileName, encoding='latin-1') as file:
+            for line in file:
+                # Generates MySQL statement and loads into database
+                tempList.append(upload.generate_upload(tableName, line))
+			
+            statement = upload.generate_statement(tableName)
+		
+            try:
+                cursor.executemany(statement, tempList)
+                conn.commit()
+                print ("Complete!")
+            except MySQLdb.Error, e:
+                print ("Error trying insert entry %s into database: %s" % (i, e))
+                pass
+
+        # PROGRESS BAR SOMEWHERE IN HERE?
+
+        print("Closing connection to database\n")
+        conn.close()
+    """
+
 
 # SET UP PROCESSES TO RUN PROGRAM
 # Sets script directory to allow absolute path naming (for Cron job)
@@ -191,168 +350,32 @@ print ("\nHEALTH CANADA DRUG PRODUCT DATABASE DATA EXTRACTION TOOL")
 print ("--------------------------------------------------------")
 print ("Created by Joshua Torrance, 2017-Feb-20\n\n")
 
+
 # Downloads zip files
 print ("DOWNLOADING DATA EXTRACTIONS ZIP FILES")
 print ("--------------------------------------")
-download_zips(suffixList, today)
+
+# download_zips(suffixList, today)
 
 
-"""
-#Extract and parse zip files
-for suffix in suffixList:
-	zipName = 'allfiles.zip' if suffix == "" else 'allfiles_' + suffix + ".zip"
-	
-	zipPath  = os.path.join(scriptDir, "DPD_Data_Extract", date, zipName)
-	
-	# Access the zip file
-	print("Opening %s\n" % zipName)
-	archive = zipfile.ZipFile(zipPath, 'r')
-	
-	# Cycles through all the files and extracts the data
-	for fileName in fileList:
-		# Generates the file name
-		fileName = fileName + ".txt" if suffix == "" else fileName + "_" + suffix + ".txt"
-		
-		# Extracts the DPD text file
-		print("Accessing %s from %s" % (fileName, zipName))
-		archive.extract(fileName)
-		
-		# Creates folder for parsed text files if necessary
-		extractLocation = os.path.join(scriptDir, "Parsed_Data", date)
-		extractLocation = os.path.normpath(extractLocation)
-		
-		if not os.path.exists(extractLocation):
-			os.mkdir(extractLocation)
-		
-		# Creates a new text file or deletes previous data within file
-		extractName = os.path.join(extractLocation, fileName)
-		extractName = os.path.normpath(extractName)
-		
-		extractFile = codecs.open(extractName, encoding='latin-1', mode='w')
-		extractFile.truncate()
-		
-		# Determining number of lines in text file for progress bar
-		length = file_len(fileName)
-		i = 1
-		n = 1
-		percent = 0
-		
-		# Opens file with 'latin-1' encoding
-		with codecs.open(fileName, encoding='latin-1') as file:
-			for line in file:
-				# Reviews and formats each line as required
-				parsedText = parse(fileName, line)
-				
-				# Writes each line to a temporary file (will ultimately upload to database)
-				try:
-					extractFile.write("%s\n" % parsedText)
-				except:
-					extractFile.write("ERROR PARSING ENTRY\n")
-				
-				# Progress bar
-				percent = 100.00 * i / length
-				
-				if percent != 100:
-					print("Parsing [%s%s] %.2f%%\r" % 
-						  ((n - 1) * "#", (51 - n) * " ", percent)),
-					sys.stdout.flush()
-					
-					n = n + 1 if percent > n * 2 else n
-				else:
-					print("Parsing [%s] Complete!" % ("#" * 50))
-				
-				i += 1
-		
-		# Closes the extract file
-		try:
-			print("Closing %s\n" % fileName)
-			os.remove(fileName)
-		except OSError, e:
-			print("Error: %s - %s." % (e.fileName, e.strerror))
-	
-	# Closes the zip archive
-	print("Closing %s\n\n" % zipName)
-	archive.close()
+# Extract the data extracts from the zip files
+print ("UNZIPPING FILES")
+print ("-------------------------------")
+
+unzip_zips(suffixList, today, fileList)
 
 
-#Connect to database
-#List of database tables
-tableList = ["comp", "comp_ap", "comp_ia",
-			 "drug", "drug_ap", "drug_ia",
-			 "form", "form_ap", "form_ia",
-			 "ingred", "ingred_ap", "ingred_ia",
-			 "package", "package_ap", "package_ia",
-			 "pharm", "pharm_ap", "pharm_ia",
-			 "route", "route_ap", "route_ia",
-			 "schedule", "schedule_ap", "schedule_ia",
-			 "status", "status_ap", "status_ia",
-			 "ther", "ther_ap", "ther_ia",
-			 "vet", "vet_ap", "vet_ia"]
-			 
-print("Establishing database connection")
-conn = MySQLdb.connect(user='USERNAME', 
-					   passwd='PASSWORD', 
-					   db='DATABASE', 
-					   host='HOST',
-					   charset='latin1',
-					   use_unicode=True)
-cursor = conn.cursor()
+# Parse the data extracts and return the data
+print ("PARSING FILES")
+print ("-------------")
 
-#Cycles through and uploads each entry to the database
-for tableName in tableList:
-	print("Uploading to '%s' table..." % tableName),
-	
-	#Truncates table to prepare for new entries
-	try:
-		cursor.execute("TRUNCATE %s" % tableName)
-		conn.commit()
-	except MySQLdb.Error, e:
-		print ("Error Truncating Table: %s" % e)
-		pass
-	
-	#Assembling path to parsed text file
-	fileName = os.path.join(scriptDir, "Parsed_Data", date, tableName + ".txt")
-	fileName = os.path.normpath(fileName)
-	
-	tempList = []
-	length = file_len(fileName)
-	i = 1
-	n = 1
-	
-	# Opens file with 'latin-1' encoding
-	with codecs.open(fileName, encoding='latin-1') as file:
-		for line in file:
-			# Generates MySQL statement and loads into database
-			tempList.append(upload.generate_upload(tableName, line))
-			
-		statement = upload.generate_statement(tableName)
-		
-		try:
-			cursor.executemany(statement, tempList)
-			conn.commit()
-			print ("Complete!")
-		except MySQLdb.Error, e:
-			print ("Error trying insert entry %s into database: %s" % (i, e))
-			pass
-		
-		
-		'''
-		# Progress bar
-		percent = 100.00 * i / length
-		
-		if percent != 100:
-			print("Uploading [%s%s] %.2f%%\r" % 
-				  ((n - 1) * "#", (51 - n) * " ", percent)),
-			sys.stdout.flush()
-			
-			n = n + 1 if percent > n * 2 else n
-		else:
-			print("Uploading [%s] Complete!\n" % ("#" * 50))
-		
-		i += 1
-		'''
-print("Closing connection to database\n")
-conn.close()
-"""
+parsedData = parse_files()
+
+#Upload the parsed files to the database
+print ("UPLOADING PARSED DATA")
+print ("---------------------")
+
+upload_data(parsedData)
+
 
 print ("Health Canada Drug Product Database Extraction Tool Finished!\n")

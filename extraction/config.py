26a3b910c94e57f3d183997c3a2642950fe0fca8
Study Buffalo <joshua@torrance.io>
2022-06-02 18:42:16 -0600

    Updating documentation for config class

--- extraction/config.py
+++ extraction/config.py
@@ -8,15 +8,24 @@ from typing import NamedTuple
 class Config:
     """Class to hold configuration details."""
     def __init__(self, config_path):
-        """Initialization details for Config class."""
-        self.sentry = None
-        self.api = None
-        self.debug = False
+        """Initialization details for Config class.
+
+            Args:
+                config_path (str):
+
+            Attributes:
+                config_path: a Path object to the script's config file.
+                sentry: A named tuple containing Sentry config details.
+                api: A named tuple containing API config details.
+        """
         self.config_path = Path(config_path)
+        self.api = None
+        self.sentry = None
 
         self._assemble_app_configuration_details()
 
     class SentryDetails(NamedTuple):
+        """Describes Sentry configuration details."""
         enable_sentry: bool
         enable_stdout: bool
         dsn: str
@@ -24,12 +33,20 @@ class Config:
         sample_rate: Decimal
 
     class APIDetails(NamedTuple):
+        """Describes HC DPD Upload API configuration details."""
         debug: bool
         url: str
         token: str
 
     def _assemble_app_configuration_details(self):
-        """Collects and assigns all the relevant configuration details."""
+        """Collects and assigns all the relevant configuration details.
+
+            Loads the config file with the provided Path. Once loaded,
+            retrieves all config details and parses into correct
+            formats and types.
+
+            Updates object attributes with these details.
+        """
         # Read the config file from the provided root
         config = configparser.ConfigParser(
             converters={'decimal': Decimal}
